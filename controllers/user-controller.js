fa496d66f3c546810cf051164ebdf15f1f137f5f
“[Akshit <akshitlakhera14@gmail.com>
2024-01-02 17:40:36 +0530

    Password will be hashed before saving in database

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 // Sign up  controller
 // const User = require('../model/User');
 const User = require('../model/user');
+const bcrypt = require('bcryptjs');
  const signup = async(req,res,next) => {
     const {name,email,password} =req.body;
     let existingUser;
@@ -12,10 +13,12 @@ const User = require('../model/user');
     if (existingUser) {
         return res.status(400).send({message:"User is already created ,login instead"})
     }
+    const saltRounds = 10; 
+    const hashedPassword  = await bcrypt.hash(password,saltRounds);
     const user = new User ({
         name, // Es6 update if name:name we write name only
         email,
-        password,
+        password:hashedPassword,
     }); 
  try {
     await user.save();
